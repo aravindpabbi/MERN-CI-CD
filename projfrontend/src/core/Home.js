e46b663fc6054ceff52af71756250b5b2eeeddd8
aravindpabbi <aravindpabbi@gmail.com>
2021-09-19 21:58:21 +0530

    testing ci

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     <Base title="Home Page" description="An MERN stack E-commerce site">
       <div className="details row">
         <div className="col-4">
-          <button className="btn sucess btn-success">Test</button>
+          <button className="btn sucess btn-success">Tst</button>
         </div>
         <div className="col-4">
           <button className="btn btn-success">Test</button>
